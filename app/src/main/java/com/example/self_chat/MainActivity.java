31a25a38d8947bf0cb74bcd082f557c561c3ff93
Eli Horovitz <eli.horovitz@cs.huji.ac.il>
2019-03-25 13:39:36 +0200

    ex1

--- app/src/main/java/com/example/self_chat/MainActivity.java
+++ app/src/main/java/com/example/self_chat/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.self_chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void updateEntry(View view) {
+        EditText input = findViewById(R.id.entryinput);
+        TextView output = findViewById(R.id.inputText);
+        output.setText(input.getText());
+        input.setText("");
     }
 }
